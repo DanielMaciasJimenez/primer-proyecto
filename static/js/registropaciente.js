4680f5481b47806c6bc463bb12cdf6b5a033c3e2
Daniel Macias <dannymac93u@gmail.com>
2024-10-23 15:17:06 -0500

    verificacion js

--- static/js/registropaciente.js
+++ static/js/registropaciente.js
@@ -1,5 +1,65 @@
 function registro_cliente(){
-    alert("Registro con éxito")
+    
+    let nombre = document.getElementById("name").value
+    let apellido = document.getElementById("lastname").value
+    let cedula= document.getElementById("c_id").value
+    let n_mascota = document.getElementById("n_pet").value
+    let animal = document.getElementById("t_animal").value
+    let edad = document.getElementById("age").value
+    let numero = document.getElementById("phone").value
+    let correo = document.getElementById("e_mail").value
+
+ 
+    validar_texto(nombre, apellido , cedula, n_mascota, animal, edad, numero, correo)
+   
+        
+  
+ }
 
 
-}
+ function validar_texto(nombre, apellido , cedula, n_mascota, animal, edad, numero, correo) {
+     if (nombre =="")  {
+   alert("Debe completar su nombre")
+ }
+
+   if (apellido == ""){
+        alert("Debe completar su apellido")
+   }
+
+    if (cedula == ""){
+        alert("Debe completar su cédula")
+    
+    }
+
+    if(n_mascota== ""){
+        alert("Debe completar su nombre de tu mascota")
+
+    }   
+
+    if(animal==""){
+        alert("Debe completar el tipo de animal que tiene")
+    }
+
+    if(edad==""){
+        alert("Debe completar la edad del su mascota")
+    }
+
+    if(numero==""){
+        alert("Debe completar su número de contacto")
+    }
+
+
+    if(correo==""){
+        alert("Debe completar su correo electrónico")
+    }
+
+
+ }
+
+ 
+
+
+
+
+
+
